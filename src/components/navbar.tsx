55ed1acee740b0faaaf72e4a8d13d5fe68ac13a0
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-05 11:50:45 +0200

    feat: make info button a dialog

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,6 +8,15 @@ import { Download, Github, Info, Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 import { generateSketchybarCode } from "@/lib/generate-config"
 import { SketchybarConfig } from "./sketchybar-editor"
 
@@ -45,17 +54,37 @@ export default function Navbar({ config }: { config: SketchybarConfig }) {
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Sketchybar Editor</h1>
         <div className="flex gap-2">
+
+          {/* Github */}
           <Link href="https://github.com/jakmaz/sketchybar-editor" target="_blank" rel="noopener noreferrer">
             <Button variant="outline" size="icon">
               <Github className="h-4 w-4" />
             </Button>
           </Link>
-          <Button variant="outline" size="icon">
-            <Info className="h-4 w-4" />
-          </Button>
+
+          {/* Info */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Info className="h-4 w-4" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Sketchybar Editor</DialogTitle>
+                <DialogDescription>
+                  Sketchybar Editor is a simple web interface to get you started with configuring your very own rice!
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          {/* Dark Mode */}
           <Button variant="outline" size="icon" onClick={toggleDarkMode}>
             {isDarkMode ? <Sun className="h-4 w-4" /> : <Moon className="h-4 w-4" />}
           </Button>
+
+          {/* Download */}
           <Button onClick={handleDownload}>
             <Download className="mr-2 h-4 w-4" />
             Download Config
